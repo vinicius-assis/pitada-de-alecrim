4927e0cd53980e4bc31b681800a7fd3e728dde5d
Vinicius Assis <vinicius.assis@hotmail.com>
2025-12-28 12:50:21 -0300

    feat: adjust configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
-
-const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
   providers: [
